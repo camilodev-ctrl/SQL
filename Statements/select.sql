da06ec7dc1df39c1cfbb573a903cf541e329f4f2
camilodev-ctrl <juancamilo.g.0512@gmail.com>
2025-06-30 19:27:02 -0500

    initial

---
+++ Statements/select.sql
@@ -0,0 +1,3 @@
+SELECT * FROM users; --: accede a la informacion de la tabla
+SELECT name FROM users; --: accede a la columna name de la tabla 
+SELECT user_id, name FROM users; --: accede a las columnas user_id y name de la tabla users
