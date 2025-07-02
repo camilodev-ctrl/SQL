622c8aff6f90056620b250175124471978792c72
camilodev-ctrl <juancamilo.g.0512@gmail.com>
2025-07-02 10:32:53 -0500

    add orderBy statement

---
+++ Statements/orderBy.sql
@@ -0,0 +1,3 @@
+SELECT * FROM users order by age; --: accede a la información de los usuarios ordenados por edad
+SELECT * FROM users order by age DESC; --: accede a la información de los usuarios ordenados por edad de forma descendente
+SELECT * FROM users WHERE email='juangallego3405@gmail.com' order by age DESC; --: accede a la información del usuario con el email
--- Statements/where.sql
+++ Statements/where.sql
@@ -1,1 +1,5 @@
 SELECT * FROM users WHERE age = 24; --: accede a la información de los usuarios cuya edad es 24
+SELECT name FROM users WHERE age = 15; --: accede a la columna name de los usuarios cuya edad es 15
+SELECT distinct name FROM users WHERE age = 24; --: accede a la columna name de los usuarios cuya edad es 24 sin duplicados
+SELECT user_id, name FROM users WHERE age = 30; --: accede a las columnas user_id y name de los usuarios cuya edad es 30
+SELECT distinct age FROM users WHERE age = 20; --: accede a la columna age de los usuarios cuya edad es 20 sin duplicados
